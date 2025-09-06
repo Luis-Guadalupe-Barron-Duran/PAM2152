d06be2f1a9f76383b71bc95d384171cce92fc64f
Luis-Guadalupe-Barron-Duran <124049120@upq.edu.mx>
2025-09-05 23:27:15 -0600

    Agregamos las librerias en la clase Main

--- Actividad_2/Aplicacion_2/src/main/java/Main.java
+++ Actividad_2/Aplicacion_2/src/main/java/Main.java
@@ -7,6 +7,12 @@
  *
  * @author LENOVO
  */
+import javax.swing.JFrame;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 public class Main {
-    
+     public static void main(String[] args) {
+       
+    }
 }
