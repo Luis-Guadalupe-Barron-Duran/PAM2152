d933de3c9b2182f0d82ec2af56b5287682cef954
Luis-Guadalupe-Barron-Duran <124049120@upq.edu.mx>
2025-09-05 23:21:27 -0600

    Enlazamos el jframe con nuestra clase INICIOPAM

--- Actividad_2/Aplicacion_2/src/main/java/App.java
+++ Actividad_2/Aplicacion_2/src/main/java/App.java
@@ -1,3 +1,6 @@
+
+import javax.swing.JOptionPane;
+
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/GUIForms/JPanel.java to edit this template
@@ -8,11 +11,13 @@
  * @author LENOVO
  */
 public class App extends javax.swing.JPanel {
-
+     
+private inicioPAM pam;
     /**
      * Creates new form App
      */
     public App() {
+            pam =  new inicioPAM(); 
         initComponents();
     }
 
@@ -25,19 +30,90 @@ public class App extends javax.swing.JPanel {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        jButton1 = new javax.swing.JButton();
+        jButton2 = new javax.swing.JButton();
+        jButton3 = new javax.swing.JButton();
+        jButton4 = new javax.swing.JButton();
+
+        jButton1.setText("Reglamento POO");
+        jButton1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton1ActionPerformed(evt);
+            }
+        });
+
+        jButton2.setText("Lineamientos Classroom");
+        jButton2.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton2ActionPerformed(evt);
+            }
+        });
+
+        jButton3.setText("Fechas Parciales");
+        jButton3.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton3ActionPerformed(evt);
+            }
+        });
+
+        jButton4.setText("Porcentajes por Parcial");
+        jButton4.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton4ActionPerformed(evt);
+            }
+        });
+
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(this);
         this.setLayout(layout);
         layout.setHorizontalGroup(
-            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 400, Short.MAX_VALUE)
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+            .addGroup(layout.createSequentialGroup()
+                .addContainerGap()
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(jButton1, javax.swing.GroupLayout.DEFAULT_SIZE, 221, Short.MAX_VALUE)
+                    .addComponent(jButton3, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                    .addComponent(jButton4, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                    .addComponent(jButton2, javax.swing.GroupLayout.DEFAULT_SIZE, 167, Short.MAX_VALUE)))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 300, Short.MAX_VALUE)
+            .addGroup(layout.createSequentialGroup()
+                .addContainerGap()
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(jButton1, javax.swing.GroupLayout.PREFERRED_SIZE, 133, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(jButton2, javax.swing.GroupLayout.PREFERRED_SIZE, 133, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                    .addComponent(jButton4, javax.swing.GroupLayout.DEFAULT_SIZE, 149, Short.MAX_VALUE)
+                    .addComponent(jButton3, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                .addContainerGap(12, Short.MAX_VALUE))
         );
     }// </editor-fold>//GEN-END:initComponents
 
+    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
+        // TODO add your handling code here:
+        JOptionPane.showMessageDialog(this, pam.ReglamentoPOO(), "Información", JOptionPane.INFORMATION_MESSAGE);
+    }//GEN-LAST:event_jButton1ActionPerformed
+
+    private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_jButton2ActionPerformed
+
+    private void jButton3ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton3ActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_jButton3ActionPerformed
+
+    private void jButton4ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton4ActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_jButton4ActionPerformed
+
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton jButton1;
+    private javax.swing.JButton jButton2;
+    private javax.swing.JButton jButton3;
+    private javax.swing.JButton jButton4;
     // End of variables declaration//GEN-END:variables
 }
--- Actividad_2/Aplicacion_2/src/main/java/inicioPAM.java
+++ Actividad_2/Aplicacion_2/src/main/java/inicioPAM.java
@@ -3,19 +3,19 @@
 public class inicioPAM {
 
     public String ReglamentoPOO() {
-        return "Reglamento POO:\n- Entregar tareas en tiempo.\n- No plagio.\n- Respeto en clase.";
+        return "Reglamento POO: 1.Se requiere 80% de asistencia para tener derecho a evaluación parcial y 80% de trabajos en clase.\n 2.Se permiten 10 minutos de tolerancia y si el alumno llega después de este tiempo puede permanecer en la clase, pero no se tomará la asistencia (Solamente en los horarios de inicio:7:00a.m y 14:00 p.m). \n 3. Las faltas deben de estar justificadas  \n 4. Los trabajos deben de enviarse de manera individual \n 5. Trabajos y tareas en tiempo y forma \n 6. Utilizar correo institucional \n 7. El plagio sera condicionado a reprobar la asignatura \n 8. No hay final para deshonestidades academicas \n 9. Tratar con respeto a los maestros y compañeros \n 10. Uso de laptops o dispositivos segun el criterio del profesor ";
     }
 
     public String LineamientosClassroom() {
-        return "Lineamientos Classroom:\n- Revisar avisos diarios.\n- Subir actividades en PDF.\n- Participar en foros.";
+        return "Lineamientos Classroom:\n- Subir entregas en PDF \n- Colocar portadas en los reportes \n- Entregar a tiempo \n Colocar nombre del alumno, nombre de la actividad y entregar a tiempo ";
     }
 
     public String FechasdeParciales() {
-        return "Fechas de Parciales:\n1er Parcial: 15 de septiembre\n2do Parcial: 20 de octubre\n3er Parcial: 25 de noviembre";
+        return "Fechas de Parciales:\n1er Parcial: 29/09/2025 \n2do Parcial: 03/11/2025\n3er Parcial: 1/12/2025";
     }
 
     public String PorcentajesporParcial() {
-        return "Porcentajes por Parcial:\n1er Parcial: 30%\n2do Parcial: 30%\n3er Parcial: 40%";
+        return "Porcentajes por Parcial:\n1er Parcial: E. Conocimiento: 40%////E. Desempeño: 20%////E. Producto: 30%////P. Integrador: 10%\n 2do Parcial: E. Conocimiento: 40%////E. Desempeño: 20%////E. Producto: 20%////P. Integrador: 20%\n 3er Parcial: E. Conocimiento: 20%////E. Desempeño: 10%////E. Producto: 40%////P. Integrador: 30%";
     }
 }
 
