0d2cd1e1a6afc5fc8c714b6d0a54ff8fb7c3f0b2
Luis-Guadalupe-Barron-Duran <124049120@upq.edu.mx>
2025-09-05 21:55:32 -0600

    Corregimos errores

--- Actividad_2/Aplicacion.java
+++ Actividad_2/Aplicacion.java
@@ -1,11 +1,11 @@
-import java.util.swing.JButton;
-import java.util.swing.JFrame;
-import java.util.swing.JPanel;
-import java.util.swing.JScrollPane;
-import java.util.swing.JTextArea;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
 
-import java.util.awt.BorderLayout;
-import java.util.awt.GridLayout;
+import java.awt.BorderLayout;
+import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
