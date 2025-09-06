6b4f5c927b2b0f60564dbfa563feb33b1705b4f5
Luis-Guadalupe-Barron-Duran <124049120@upq.edu.mx>
2025-09-05 23:25:09 -0600

    Iniciamos nuestros botones y colocamos los metodos de cada uno

--- Actividad_2/Aplicacion_2/src/main/java/App.java
+++ Actividad_2/Aplicacion_2/src/main/java/App.java
@@ -94,19 +94,22 @@ private inicioPAM pam;
 
     private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
         // TODO add your handling code here:
-        JOptionPane.showMessageDialog(this, pam.ReglamentoPOO(), "Información", JOptionPane.INFORMATION_MESSAGE);
+        JOptionPane.showMessageDialog(this, pam.ReglamentoPOO(), "Reglamento", JOptionPane.INFORMATION_MESSAGE);
     }//GEN-LAST:event_jButton1ActionPerformed
 
     private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
         // TODO add your handling code here:
+        JOptionPane.showMessageDialog(this, pam.LineamientosClassroom(), "LineamientosClassroom", JOptionPane.INFORMATION_MESSAGE);
     }//GEN-LAST:event_jButton2ActionPerformed
 
     private void jButton3ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton3ActionPerformed
         // TODO add your handling code here:
+        JOptionPane.showMessageDialog(this, pam.FechasdeParciales(), "FechasdeParciales", JOptionPane.INFORMATION_MESSAGE);
     }//GEN-LAST:event_jButton3ActionPerformed
 
     private void jButton4ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton4ActionPerformed
         // TODO add your handling code here:
+        JOptionPane.showMessageDialog(this, pam.PorcentajesporParcial(), "PorcentajesporParcial", JOptionPane.INFORMATION_MESSAGE);
     }//GEN-LAST:event_jButton4ActionPerformed
 
 
