8bdea6b0de1afcdef5d6b93c26c25b68b43f4bbf
Luis-Guadalupe-Barron-Duran <124049120@upq.edu.mx>
2025-09-05 23:31:27 -0600

    Importamos librerias y creamos el Jframe en el metodo main

--- Actividad_2/Aplicacion_2/src/main/java/Main.java
+++ Actividad_2/Aplicacion_2/src/main/java/Main.java
@@ -6,13 +6,18 @@
 /**
  *
  * @author LENOVO
- */
+ */import javax.swing.SwingUtilities;
 import javax.swing.JFrame;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class Main {
      public static void main(String[] args) {
-       
-    }
+         JFrame frame = new JFrame("Aplicación 1");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+      frame.setContentPane(new App()); 
+        frame.pack();
+        frame.setLocationRelativeTo(null); 
+        frame.setVisible(true); 
+     }
 }
