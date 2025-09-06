a90c88f0d355744722fe86bc068b0755b3b5cfb2
Luis-Guadalupe-Barron-Duran <124049120@upq.edu.mx>
2025-09-05 21:52:26 -0600

    Importamos librerias de Java swing y awt para los botones

---
+++ Actividad_2/Aplicacion.java
@@ -0,0 +1,14 @@
+import java.util.swing.JButton;
+import java.util.swing.JFrame;
+import java.util.swing.JPanel;
+import java.util.swing.JScrollPane;
+import java.util.swing.JTextArea;
+
+import java.util.awt.BorderLayout;
+import java.util.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class Aplicacion {
+    
+}
