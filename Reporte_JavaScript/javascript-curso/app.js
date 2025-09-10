3b6e4cf322b47ea78d575e39c97cb6cc380f0581
Luis-Guadalupe-Barron-Duran <124049120@upq.edu.mx>
2025-09-09 18:10:40 -0600

    Tipos de datos

---
+++ Reporte_JavaScript/javascript-curso/app.js
@@ -0,0 +1,20 @@
+//Tipos de datos
+"Hello World"; //String
+'Hello World' //String
+
+1000000000 //number 
+-2.3 //number
+
+true //boolean
+false //boolean
+
+// array
+[1,2,3,4,5] 
+['joe', 'ryan', 'martha']
+
+// object
+'ryan'
+70.4
+14
+true
+
