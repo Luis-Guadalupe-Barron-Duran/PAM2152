379b00c98d61b838a95e4e75d4cd75f825174abb
Luis-Guadalupe-Barron-Duran <124049120@upq.edu.mx>
2025-09-26 19:55:15 -0600

    EjercicioA
    1. Primero creamos el archivo donde se encuentra la función de restar (util.js)
    2. Creamos una funcion restar que nos retorne la resta de dos numeros a y b. Ademas exportamos la funcion desde el principio al declararla.
    
    3. Creamos el archivo main y le importamos el archivo utils y simplemente con un cosole.log imprimimos la resta, asignando los dos  numeros que queremos restar

---
+++ Practica3/main.js
@@ -0,0 +1,5 @@
+import * as math from './utils.js';
+console.log("La rsta es: "+math.restar(3, 2));
+console.log("La resta es: "+math.restar(4, 8));
+console.log("La resta es: "+math.restar(45, 17));
+
