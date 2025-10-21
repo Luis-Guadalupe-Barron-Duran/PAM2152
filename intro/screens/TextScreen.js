eec8393406ae61d2f321fb1ce63a663cbe2a0cc7
Luis-Guadalupe-Barron-Duran <124049120@upq.edu.mx>
2025-10-20 20:22:22 -0600

    TextInput & Alert: Completada

--- intro/screens/TextScreen.js
+++ intro/screens/TextScreen.js
@@ -1,14 +1,103 @@
-import { Text, StyleSheet, View } from 'react-native'
-
-
+import { Text, StyleSheet, View, Button, TextInput, Alert } from 'react-native'
+import React, {useState} from 'react'
 export default function BotonesScreen() {
+  const [nombre, setNombre] = useState('');
+  const [password, setPassword] = useState('');
+  const [telefono, setTelefono] = useState('');
 
+  const mostrarAlerta= () => {
+     if(nombre.trim() === '' ||password.trim() === '' || telefono.trim() === ''){
+      Alert.alert("Error favor complete todos los campos (mobile)");
+      alert("Error favor complete todos los campos (web)");
+      } else {
+        //alert en mobile
+      Alert.alert("Datos ingresados"+
+         `Nombre: ${nombre}\n
+         Password: ${password}\n
+         Telefono: ${telefono}`
+        );
+        //alert en web
+         alert("Datos ingresados: \n"+`Nombre: ${nombre}\nPassword: ${password}\nTelefono: ${telefono}`
+        );
+     }
+
+  }
     return (
-      <View>
-        <Text>Proximamente por Dulce Ariadna</Text>
+      <View style={styles.container}>
+        <Text style={styles.titulo}>TextInput & Alert</Text>
+        <Text style={styles.etiqueta}>Nombre: </Text>
+        <TextInput style={styles.input}
+          placeholder='Escribe tu nombre aqui'
+          value={nombre}
+          onChangeText={setNombre}
+          ></TextInput>
+
+          <Text Text style={styles.etiqueta}>Contraseña: </Text>
+        <TextInput style={styles.input}
+          placeholder='Escribe tu password aqui'
+          secureTextEntry={true}
+          value={password}
+          onChangeText={setPassword}
+          ></TextInput>
+
+          <Text Text style={styles.etiqueta}>Telefono: </Text>
+        <TextInput style={styles.input}
+          placeholder='Escribe tu telefono aqui'
+          keyboardType='phone-pad'
+          value={telefono}
+          onChangeText={setTelefono}
+          ></TextInput>
+
+          <Button
+            color='#000000ff'
+            title='Mostrar datos'
+            onPress={mostrarAlerta}
+          
+          />
+
+
+
+         
       </View>
     )
   
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+
+  container:{
+    flex:1,
+    backgroundColor:'#85a8edff',
+    alignItems:'center',
+    justifyContent:'center',
+    
+  },
+
+  titulo:{
+    fontFamily:'Times New Roman',
+    fontSize:30,
+    color:'#000000',
+    fontWeight:'bold',
+   // fontStyle:'italic',
+   textDecorationLine:'underline',
+   marginBottom:20,
+  },
+
+  etiqueta:{
+    fontSize:20,
+   // color:'#000000',
+    marginBottom:5,
+    marginTop:10,
+  },
+
+  input:{
+    width:'50%',//ocupa el ancho disponible
+    borderWidth:1,//grosor del borde
+    borderColor:'#000000ff',//color del borde
+    borderRadius:8,//para los redondeados
+    padding:10,//espacio interno
+    marginBottom:15,//
+    backgroundColor:'#b3b3b3ff',
+  },
+
+})
